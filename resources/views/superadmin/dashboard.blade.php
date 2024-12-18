2c87fc19a03a273370657019b4823a41de285528
esoftzahid <esoft123456@gmail.com>
2024-12-18 12:55:56 +0600

    dashboard page modified

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -1,109 +1,110 @@
 @extends('layouts.superadmin')
 @section('title')
-    <title>Dashboard | Super Admin | Nurjahan Bazar</title>
+    <title>Dashboard | Super Admin | NRB World</title>
 @endsection
 
 @section('main')
     <div class="content">
         <div class="container-fluid">
-            <!-- Summary Cards -->
-            <div class="row">
-                <!-- Total Sale -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-warning card-header-icon">
-                            <div class="card-icon">
-                                <i class="material-icons">content_copy</i>
-                            </div>
-                            <p class="card-category">Total Sale</p>
-                            <h3 class="card-title">{{ number_format($totalSale, 2) }} <small>tk</small></h3>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <a href="{{ route('super.total.sale') }}">View More...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Total Collection -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-success card-header-icon">
-                            <div class="card-icon">
-                                <i class="material-icons">store</i>
-                            </div>
-                            <p class="card-category">Total Collection</p>
-                            <h3 class="card-title">{{ number_format($totalCollection, 2) }} <small>tk</small></h3>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <a href="{{ route('super.total.collection') }}">View More...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Total Due -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-danger card-header-icon">
-                            <div class="card-icon">
-                                <i class="material-icons">info_outline</i>
-                            </div>
-                            <p class="card-category">Total Due</p>
-                            <h3 class="card-title">{{ number_format($totalDue, 2) }} <small>tk</small></h3>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <a href="{{ route('super.total.due') }}">View More...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Total Stock -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-info card-header-icon">
-                            <div class="card-icon">
-                                <i class="material-icons">content_copy</i>
-                            </div>
-                            <p class="card-category">Total Stock</p>
-                            <h3 class="card-title">{{ $totalStock }} <small>items</small></h3>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <a href="{{ route('product.index') }}">View More...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--            <!-- Summary Cards -->--}}
+{{--            <div class="row">--}}
+{{--                <!-- Total Sale -->--}}
+{{--                <div class="col-lg-3 col-md-6 col-sm-6">--}}
+{{--                    <div class="card card-stats">--}}
+{{--                        <div class="card-header card-header-warning card-header-icon">--}}
+{{--                            <div class="card-icon">--}}
+{{--                                <i class="material-icons">content_copy</i>--}}
+{{--                            </div>--}}
+{{--                            <p class="card-category">Total Sale</p>--}}
+{{--                            <h3 class="card-title">{{ number_format($totalSale, 2) }} <small>tk</small></h3>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-footer">--}}
+{{--                            <div class="stats">--}}
+{{--                                <a href="{{ route('super.total.sale') }}">View More...</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- Total Collection -->--}}
+{{--                <div class="col-lg-3 col-md-6 col-sm-6">--}}
+{{--                    <div class="card card-stats">--}}
+{{--                        <div class="card-header card-header-success card-header-icon">--}}
+{{--                            <div class="card-icon">--}}
+{{--                                <i class="material-icons">store</i>--}}
+{{--                            </div>--}}
+{{--                            <p class="card-category">Total Collection</p>--}}
+{{--                            <h3 class="card-title">{{ number_format($totalCollection, 2) }} <small>tk</small></h3>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-footer">--}}
+{{--                            <div class="stats">--}}
+{{--                                <a href="{{ route('super.total.collection') }}">View More...</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- Total Due -->--}}
+{{--                <div class="col-lg-3 col-md-6 col-sm-6">--}}
+{{--                    <div class="card card-stats">--}}
+{{--                        <div class="card-header card-header-danger card-header-icon">--}}
+{{--                            <div class="card-icon">--}}
+{{--                                <i class="material-icons">info_outline</i>--}}
+{{--                            </div>--}}
+{{--                            <p class="card-category">Total Due</p>--}}
+{{--                            <h3 class="card-title">{{ number_format($totalDue, 2) }} <small>tk</small></h3>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-footer">--}}
+{{--                            <div class="stats">--}}
+{{--                                <a href="{{ route('super.total.due') }}">View More...</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- Total Stock -->--}}
+{{--                <div class="col-lg-3 col-md-6 col-sm-6">--}}
+{{--                    <div class="card card-stats">--}}
+{{--                        <div class="card-header card-header-info card-header-icon">--}}
+{{--                            <div class="card-icon">--}}
+{{--                                <i class="material-icons">content_copy</i>--}}
+{{--                            </div>--}}
+{{--                            <p class="card-category">Total Stock</p>--}}
+{{--                            <h3 class="card-title">{{ $totalStock }} <small>items</small></h3>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-footer">--}}
+{{--                            <div class="stats">--}}
+{{--                                <a href="{{ route('product.index') }}">View More...</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
-            <!-- Charts -->
-            <div class="row">
-                <!-- Daily Sales Chart -->
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header card-header-success">
-                            <h4 class="card-title">Daily Sales</h4>
-                        </div>
-                        <div class="card-body">
-                            <canvas id="dailySalesChart"></canvas>
-                        </div>
-                    </div>
-                </div>
+{{--            <!-- Charts -->--}}
+{{--            <div class="row">--}}
+{{--                <!-- Daily Sales Chart -->--}}
+{{--                <div class="col-md-6">--}}
+{{--                    <div class="card">--}}
+{{--                        <div class="card-header card-header-success">--}}
+{{--                            <h4 class="card-title">Daily Sales</h4>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-body">--}}
+{{--                            <canvas id="dailySalesChart"></canvas>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
-                <!-- Daily Collections Chart -->
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header card-header-warning">
-                            <h4 class="card-title">Daily Collections</h4>
-                        </div>
-                        <div class="card-body">
-                            <canvas id="dailyCollectionsChart"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--                <!-- Daily Collections Chart -->--}}
+{{--                <div class="col-md-6">--}}
+{{--                    <div class="card">--}}
+{{--                        <div class="card-header card-header-warning">--}}
+{{--                            <h4 class="card-title">Daily Collections</h4>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-body">--}}
+{{--                            <canvas id="dailyCollectionsChart"></canvas>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+
         </div>
     </div>
 
